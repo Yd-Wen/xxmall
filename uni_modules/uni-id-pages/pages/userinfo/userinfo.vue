8b6705934aab812ca7395d9783d1bae17c089d30
Yindong Wen <wenyindong@163.com>
2026-03-10 21:34:06 +0800

    update pages: user-info

--- uni_modules/uni-id-pages/pages/userinfo/userinfo.vue
+++ uni_modules/uni-id-pages/pages/userinfo/userinfo.vue
@@ -248,6 +248,8 @@ const uniIdCo = uniCloud.importObject("uni-id-co")
 					})
 					if (res.errCode == 0) {
 						this.inviteData.myInviteCode = res.myInviteCode
+						this.shareData.content.inviteCode = codeRes.myInviteCode
+						this.shareData.params.inviteCode = codeRes.myInviteCode
 						uni.showToast({
 							title: res.errMsg,
 							icon: 'success'
